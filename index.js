69b9a1fe3a189407ef4208542768855606b94288
Nick Lorenson <nick.lorenson@corrdyn.com>
2026-08-18 14:00:56 -0500

    Bake public runtime config into the function entrypoint

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 const { onRequest } = require('firebase-functions/v2/https')
 const { defineSecret } = require('firebase-functions/params')
+
+process.env.GOOGLE_CLIENT_ID ??= '739923415388-vtq7qsf2d4u6q9lfis4i5ev3qjhpg68g.apps.googleusercontent.com'
+process.env.GOOGLE_WORKSPACE_DOMAIN ??= 'treeconservationcorps.org'
+process.env.FRONTEND_ORIGIN ??= 'http://localhost:5173'
+
 const app = require('./app')
 
 const squarespaceApiKey = defineSecret('SQUARESPACE_API_KEY')
